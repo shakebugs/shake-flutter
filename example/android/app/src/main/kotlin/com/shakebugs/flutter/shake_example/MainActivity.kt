dfe5373d4bc62a1b3835d53728bbfd8fe870b1ae
Mislav Stanić <mislav.stanic@decode.agency>
2020-07-14 14:53:13 +0200

    Add touch tracking

--- example/android/app/src/main/kotlin/com/shakebugs/flutter/shake_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/shakebugs/flutter/shake_example/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.shakebugs.flutter.shake_example
 
+import android.view.MotionEvent
+import com.shakebugs.shake.Shake
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        Shake.handleTouchEvent(ev, this)
+        return super.dispatchTouchEvent(ev)
+    }
 }
